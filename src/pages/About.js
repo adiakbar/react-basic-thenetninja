fd008e65047382cee1262f6f27d687bfd2e84a23
adiakbar <adi.akbar91@gmail.com>
2019-12-07 07:41:35 +0700

    create home, about and navbar pages component

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About Page</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quidem praesentium hic nisi odio quasi harum sed. Vel deserunt nulla blanditiis cumque, 
+        quidem praesentium voluptas exercitationem corrupti possimus maiores? Sit, tenetur!</p>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home Page</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quidem praesentium hic nisi odio quasi harum sed. Vel deserunt nulla blanditiis cumque, 
+        quidem praesentium voluptas exercitationem corrupti possimus maiores? Sit, tenetur!</p>
+    </div>
+  )
+}
+
+export default Home;
