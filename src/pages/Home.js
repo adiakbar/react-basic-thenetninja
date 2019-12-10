e3aa159e59a39f48ae29dbe7b4d0a958a3f7e7c8
adiakbar <adi.akbar91@gmail.com>
2019-12-10 21:17:50 +0700

    install axios and update home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home Page</h4>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Quidem praesentium hic nisi odio quasi harum sed. Vel deserunt nulla blanditiis cumque, 
-        quidem praesentium voluptas exercitationem corrupti possimus maiores? Sit, tenetur!</p>
-    </div>
-  )
+import axios from 'axios';
+
+class Home extends Component {
+  state = {
+    posts: []
+  }
+  
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data.slice(0,10)
+        })
+      })
+  }
+  
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{ post.title }</span>
+              <p>{ post.body }</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No Posts yet</div>
+    )
+
+    return (
+      <div className="container">
+        <h4 className="center">Home Page</h4>
+        { postList }
+      </div>
+    )
+  }
 }
 
 export default Home;
