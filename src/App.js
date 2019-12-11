7f5ee13ebc8c1b1432cb561560e322ba69ffbc3c
adiakbar <adi.akbar91@gmail.com>
2019-12-11 21:24:46 +0700

    finish learn route parameter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import AppTodo from './pages/Todo/AppTodo';
+import Post from './pages/Post';
 
 class App extends Component {
 
@@ -16,6 +17,7 @@ class App extends Component {
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/todos' component={AppTodo} />
+          <Route path='/:post_id' component={Post} />
       </BrowserRouter>
     )
   }
