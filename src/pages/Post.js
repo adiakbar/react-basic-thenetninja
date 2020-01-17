a9e7368e4a2931fbe9104c34f136d6c4c68c42e8
adi akbar <adi.akbar91@gmail.com>
2020-01-17 20:33:12 +0700

    add action creator on dispatch post

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import { connect } from 'react-redux';
-
+import { deletePost } from '../actions/postAction';
 
 class Post extends Component {
   
@@ -59,7 +59,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     deletePost: (id) => {
-      dispatch({ type: 'DELETE_POST', id: id })
+      dispatch(deletePost(id))
     }
   }
 }
